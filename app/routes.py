fcd6ca57001881c51dfc92adf7779c3e47e64253
Alexander Nikolas Reuber <info@unconnect.net>
2020-08-28 20:39:49 +0200

    added error messages to login.html, url_for added

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import crm
 from datetime import date
 from app.forms import LoginForm
@@ -49,7 +49,7 @@ def login():
     if form.validate_on_submit():
         flash(f'Login requested for user {form.username.data},'
               f'remember_me={form.remember_me.data}')
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html',
                            title='Sign In',
                            the_year=the_year,
