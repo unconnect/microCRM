3de7c50c2c744e43cfae66c34d2b255c9d044faf
Alexander Nikolas Reuber <info@unconnect.net>
2020-08-28 20:39:09 +0200

    added template_dir to config.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from config import Config
 
+
 crm = Flask(__name__)
 crm.config.from_object(Config)
+
+template_dir = crm.config['TEMPLATE_DIR']
+crm.template_folder = template_dir
 
 from app import routes
--- config.py
+++ config.py
@@ -7,3 +7,5 @@ class Config(object):
     """
     # Reads environmental variable when none is given uses static value
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-nerver-guess-this'
+    TEMPLATE_DIR = os.environ.get('TEMPLATE_DIR') or os.path.abspath(
+        'app/templates')
